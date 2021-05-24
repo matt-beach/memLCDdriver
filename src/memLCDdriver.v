99b920b8a2a0541d036295a75afad729bdbda6c0
zerotoasic <zerotoasic@zerotoasic.vm.com>
2021-05-24 18:38:33 +0200

    top level

--- src/memLCDdriver.v
+++ src/memLCDdriver.v
@@ -4,6 +4,8 @@ module memLCDdriver (
     // System Control Signals
     input   i_clk, // 100MHz
     input   i_reset,
+    input   i_vcom_start,
+    // For Logic analizer
     output  o_wfull,
     output  o_wfull_almost,
     output  o_rempty,
@@ -12,6 +14,7 @@ module memLCDdriver (
     input   i_spi_mosi,
     input   i_spi_cs_n,
     input   i_spi_clk,
+    output  o_spi_cts,
     // Memory LCD signals
     output  o_va,
     output  o_vb,
@@ -27,19 +30,18 @@ module memLCDdriver (
     
     wire        w_clk_vcom;
     wire [7:0]  w_spi_data;
-    wire [7:0]  w_lcd_data;
+    wire [5:0]  w_lcd_data;
     wire        w_spi_dataValid;
     wire        w_rclk;
     wire        w_rinc;
     wire        w_rempty;
     wire        w_wfull;
-
-    // Generate 60Hz clock for VCOM, VA, and VB
-    clockdiv #(.DIVISOR(16666666), .SIZE(20)) vcom_clockdiv (
-        .i_clk(i_clk), 
-        .i_reset(i_reset), 
-        .o_clk_div(w_clk_vcom)
-    );
+    wire        w_rempty_almost;
+    wire        w_wfull_almost;
+    reg         r_spi_cts;
+    reg         r_va;
+    reg         r_vb;
+    reg         r_vcom;
 
     // SPI Reciever
     spi_s spi_s(
@@ -53,21 +55,21 @@ module memLCDdriver (
     );
 
     // Syncronous FIFO
-    sfifo #(.DATA_WIDTH(8), .ADDR_WIDTH(6)) sfifo (
+    sfifo #(.DATA_WIDTH(6), .ADDR_WIDTH(6)) sfifo (
         .i_clk(i_clk),
         .i_reset(i_reset),
 	    .o_rdata(w_lcd_data),
-	    .i_wdata(w_spi_data),
+	    .i_wdata(w_spi_data[5:0]),
 	    .i_rinc(w_rinc),
 	    .i_winc(w_spi_dataValid),
 	    .o_wfull(w_wfull),
-	    .o_wfull_almost(o_wfull_almost),
+	    .o_wfull_almost(w_wfull_almost),
 	    .o_rempty(w_rempty),
-	    .o_rempty_almost(o_rempty_almost)
+	    .o_rempty_almost(w_rempty_almost)
     );
 
     // LCD controller
-    memlcd_fsm #(.DATA_WIDTH(8)) memlcd_fsm (
+    memlcd_fsm #(.DATA_WIDTH(6)) memlcd_fsm (
         .i_clk(i_clk),
         .i_reset(i_reset),
         .i_data(w_lcd_data),
@@ -82,10 +84,42 @@ module memLCDdriver (
         .o_rgb(o_rgb)
     );
 
-    assign o_vcom   = w_clk_vcom;
-    assign o_va     = !w_clk_vcom;
-    assign o_vb     = w_clk_vcom;
-    assign o_rempty = w_rempty;
-    assign o_wfull  = w_wfull;
+    // Clear to Send
+    always @(posedge i_clk or posedge i_reset) begin
+        if (i_reset) begin
+            r_spi_cts <= 1'b1;
+        end else if (w_rempty_almost) begin
+            r_spi_cts <= 1'b1;
+        end else if (w_wfull_almost) begin
+            r_spi_cts <= 1'b0;
+        end
+    end
+    assign o_spi_cts    =   r_spi_cts;
 
+
+    // Generate 60Hz clock for VCOM, VA, and VB
+    clockdiv #(.DIVISOR(1666666), .SIZE(20)) vcom_clockdiv (
+        .i_clk(i_clk), 
+        .i_reset(i_reset || !i_vcom_start), 
+        .o_clk_div(w_clk_vcom)
+    );
+    always @(*) begin
+        if (i_vcom_start) begin
+            r_vcom   <= !w_clk_vcom;
+            r_va     <= w_clk_vcom;
+            r_vb     <= !w_clk_vcom;
+        end else begin
+            r_vcom   <= 1'b0;
+            r_va     <= 1'b0;
+            r_vb     <= 1'b0;
+        end
+    end
+    assign o_vcom   = r_vcom;
+    assign o_va     = r_va;
+    assign o_vb     = r_vb;
+
+    assign o_rempty         = w_rempty;
+    assign o_wfull          = w_wfull;
+    assign o_rempty_almost  = w_rempty_almost;
+    assign o_wfull_almost   = w_wfull_almost;
 endmodule
--- test/test_memLCDdriver.py
+++ test/test_memLCDdriver.py
@@ -5,6 +5,7 @@ import random
 
 async def reset(dut):
     dut.i_reset     <= 1
+    dut.i_vcom_start <= 0
     dut.i_spi_mosi  <= 0
     dut.i_spi_cs_n   <= 1
     dut.i_spi_clk   <= 0
@@ -27,7 +28,6 @@ async def send_byte(dut, data):
     dut.i_spi_cs_n   <= 1 # end data packet
     await ClockCycles(dut.i_clk, 8)
 
-
 async def send_word(dut, data):
     dut.i_spi_cs_n   <= 0 # start data packet
     for i in range(32):
@@ -43,6 +43,19 @@ async def send_word(dut, data):
     dut.i_spi_cs_n   <= 1 # end data packet
     await ClockCycles(dut.i_clk, 8)
 
+async def send_lcd_line(dut, num_lines):
+    # Test sending byte packets when FIFO is not full
+    data = 0x01
+    packet_count = 0
+    while (packet_count < 120*num_lines): # send image frames (120*640 for a frame)
+        if (dut.o_spi_cts == 1):
+            packet_count = packet_count + 1
+            await send_byte(dut, data)
+            data = data + 0x01
+            if (data > 0x3F):
+                data = 0x01 
+        await ClockCycles(dut.i_clk, 1)
+
 @cocotb.test()
 async def test_all(dut):
     clock = Clock(dut.i_clk, 10, units="ns") # 100MHz
@@ -50,27 +63,12 @@ async def test_all(dut):
     cocotb.fork(clock.start())
 
     await reset(dut)
-
-    # Test sending byte packets when FIFO is not full
-    data = 0x01
-    packet_count = 0
-    send_packet = 0
-    while (packet_count < 120*640): # send image frames (120*640 for a frame)
-        if (dut.o_wfull == 1):
-            send_packet = 0
-        if (dut.o_rempty == 1): # change to o_rempty_almost
-            send_packet = 1
-
-        if (send_packet == 1):
-            packet_count = packet_count + 1
-            await send_byte(dut, data)
-            data = data + 0x01
-            if (data > 0x3F):
-                data = 0x01 
-
+    
+    dut.i_vcom_start <= 1
+    
+    await send_lcd_line(dut, 128)
+    while (dut.o_rempty == 0):
         await ClockCycles(dut.i_clk, 1)
 
-    # Flush the FIFO and finish signaling
-    while (dut.o_rempty == 0):
-        await ClockCycles(dut.i_clk, 10)
-    await ClockCycles(dut.i_clk, 500000) # 5ms
+    
+    await ClockCycles(dut.i_clk, 5000) # .050ms
